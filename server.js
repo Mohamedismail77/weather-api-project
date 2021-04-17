efbff62498bb323f15b388b9c7f22fb30b933fc6
Mohamed Ismail <mohamedismail77@yahoo.com>
2021-04-17 08:14:33 +0200

    complete project

---
+++ server.js
@@ -0,0 +1,37 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// Start up an instance of app
+const app  = express();
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3000;
+
+
+app.get('/projectData',(request,response)=>{
+    response.send(projectData);
+});
+
+app.post('/projectData',(request,response)=>{
+    projectData = request.body;
+    response.send(projectData);
+});
+
+app.listen(port,()=>{
+    console.log(`server up and running: localhost:${port}`);
+});
+
---
+++ website/app.js
@@ -0,0 +1,66 @@
+/* Global Variables */
+const appId = "3e15e3ed3633b0ac40ef1c8d5945a468";
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+const generateButton = document.querySelector('#generate');
+const userReponseInput = document.querySelector('#feelings');
+const output = document.querySelector('#entryHolder');
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const getWeatherData = async (url = '',zipCode,apiKey)=>{
+
+    const response = await fetch(`${url}?zip=${zipCode}&appid=${apiKey}`, {
+        method:'GET',
+        credentials: 'same-origin',
+    });
+
+    try{
+        const data = await response.json();
+        return data;
+    }catch(error){
+       console.log(error);
+    }
+}
+
+
+const saveUserResponse = async (url='/projectData',body={})=> {
+
+
+    const response = await fetch(url,{
+        method:'POST',
+        credentials: 'same-origin',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    try{
+        const data = await response.json();
+        return data;
+    } catch(error) {
+        console.log(error);
+    }
+
+}
+
+generateButton.addEventListener('click',()=>{
+    const zipCode = document.querySelector('#zip');
+    getWeatherData(baseUrl,zipCode.value,appId)
+    .then((data)=>{
+        let body = {
+            temperature:data.main.temp,
+            date:Date(),
+            user_response:userReponseInput.value
+        };
+        saveUserResponse('/projectData',body)
+        .then(updateUi);
+    });
+    
+});
+
+
+const updateUi = (data)=>{
+    output.innerText = `Temperature:${data.temperature}\nDate:${data.date}\nUser Feeling:${data.user_response}`;
+}
